0abf5e76c23da859f2d8ab9f9234c4d89da25ea3
aschip <aschip@outlook.com>
2020-01-11 10:10:53 +1300

    trying out bind()

---
+++ src/app/address/address.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from "@angular/core";
+
+declare var AddressFinder: any;
+
+@Component({
+  selector: "app-address",
+  templateUrl: "./address.component.html",
+  styleUrls: ["./address.component.css"]
+})
+export class AddressComponent implements OnInit {
+  ngOnInit() {
+    let script = document.createElement("script");
+    script.src = "https://api.addressfinder.io/assets/v3/widget.js";
+    script.async = true;
+    script.onload = this.initAf;
+    document.body.appendChild(script);
+  }
+
+  initAf() {
+    let widget = new AddressFinder.Widget(
+      document.getElementById("addrs"),
+      "ADDRESSFINDER_DEMO_KEY",
+      "NZ",
+      {}
+    );
+    var vm = this;
+    /* widget documentation is here: https://addressfinder.nz/docs/widget_docs/ */
+    widget.on("address:select",vm.addressSelected);
+      //
+
+    /* this works fine but cannot get angular component inside the function*/
+    /*widget.on("result:select", function(fullAddress, metaData) {
+      console.log(metaData);
+    });*/
+  
+}
+
+public addressSelected = (function(address, metaData) {
+    console.log(metaData);
+    }).bind(this);
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import {AddressComponent} from './address/address.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent ],
+  declarations: [ AppComponent, AddressComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
